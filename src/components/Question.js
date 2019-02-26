9cd75612613b3da40709de26b5eed37d6ae77045
Harish Balakrishnan <harish.balakrishnan.93@gmail.com>
2019-02-27 03:35:54 +0530

    Added Results component

--- src/components/Question.js
+++ src/components/Question.js
@@ -34,7 +34,12 @@ class Question extends Component {
   render() {
 
     const {checkedValue,toHome} = this.state
-    const {users,pageAvailable,questionAnswered,question} = this.props
+    const {users,pageAvailable,questionAnswered,question,authedUser} = this.props
+    const optOneVotes = pageAvailable? question.optionOne.votes.length : 0
+    const optTwoVotes = pageAvailable ? question.optionTwo.votes.length : 0
+    const totalVotes = optOneVotes + optTwoVotes
+    const optOnePercentage = pageAvailable ? (optOneVotes / totalVotes) * 100 : 0
+    const optTwoPercentage = pageAvailable ? (optTwoVotes / totalVotes) * 100 : 0
 
     if(toHome === true){
       return (
@@ -44,12 +49,80 @@ class Question extends Component {
 
     if(!(pageAvailable)){
       return (
-        <div>404 Page Not Found</div>
+        <div className='center'>
+          <h3>404 Error : Question Not Found</h3>
+        </div>
       )
     }
     else if(questionAnswered){
       return(
-        <div>Results Dabba </div>
+        <div className='question'>
+          <div className='row-data'>
+            <div className='question-data'>
+              <div>
+                <span>{users[question.author].name} asked:</span>
+              </div>
+            </div>
+            <div className='poll-avatar-holder'>
+              <img
+                src={users[question.author].avatarURL}
+                alt={`Avatar of ${question.author}`}
+                className='poll-avatar'
+                />
+            </div>
+          </div>
+          <div className='row-data-1'>
+            <div className='question-data'>
+              <div>
+                <span className='center'>Would You Rather...</span>
+                <hr/>
+
+                <div className='center'>
+                  {
+                    question.optionOne.votes.includes(authedUser)
+                    ?
+                    <div className="list-group-item list-group-item-success">
+                      <span className="badge"><span class="glyphicon glyphicon-star"></span>You Voted</span>
+                      <p>{question.optionOne.text}</p>
+                      <div className='center progress'>
+                        <div className='center progress-bar bg-success' role='progressbar' style={{width: `${optOnePercentage}%`}} aria-valuenow={optOnePercentage} aria-valuemin='0' aria-valuemax='100'>{optOneVotes} out of {totalVotes} votes</div>
+                      </div>
+                    </div>
+                    :
+                    <div className="list-group-item">
+                      <p>{question.optionOne.text}</p>
+                      <div className='center progress'>
+                        <div className='center progress-bar bg-success' role='progressbar' style={{width: `${optOnePercentage}%`}} aria-valuenow={optOnePercentage} aria-valuemin='0' aria-valuemax='100'>{optOneVotes} out of {totalVotes} votes</div>
+                      </div>
+                    </div>
+                  }
+                </div>
+
+                <div className='center'>
+                  {
+                    question.optionTwo.votes.includes(authedUser)
+                    ?
+                    <div className="list-group-item list-group-item-success">
+                      <span className="badge"><span class="glyphicon glyphicon-star"></span>You Voted</span>
+                      <p>{question.optionTwo.text}</p>
+                      <div className='center progress'>
+                        <div className='center progress-bar bg-success' role='progressbar' style={{width: `${optTwoPercentage}%`}} aria-valuenow={optTwoPercentage} aria-valuemin='0' aria-valuemax='100'>{optTwoVotes} out of {totalVotes} votes</div>
+                      </div>
+                    </div>
+                    :
+                    <div className="list-group-item">
+                      <p>{question.optionTwo.text}</p>
+                      <div className='center progress'>
+                        <div className='center progress-bar bg-success' role='progressbar' style={{width: `${optTwoPercentage}%`}} aria-valuenow={optTwoPercentage} aria-valuemin='0' aria-valuemax='100'>{optTwoVotes} out of {totalVotes} votes</div>
+                      </div>
+                    </div>
+                  }
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
       )
     }
     else{
